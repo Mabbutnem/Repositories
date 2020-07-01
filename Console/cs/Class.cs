1b692f2df46b1b9ec93e9d33442952b8f8e6194b
Mabbutnem <thibault.harel1@gmail.com>
2020-07-01 21:34:57 +0200

    subtype object added outside list

--- Console/cs/Class.cs
+++ Console/cs/Class.cs
@@ -9,10 +9,14 @@ namespace RepositoriesConsole
    {
       public string Field1 { get; set; }
       public IChildClass[] List { get; set; }
+      public IChildClass Child { get; set; }
 
       public override string ToString()
       {
-         return string.Format("#Field1: {0}, List: [{1}]#", Field1, List.Select(c => c.ToString()).Aggregate((str1, str2) => str1 + ", " + str2));
+         return string.Format("#Field1: {0}, List: [{1}], Child: {2}#",
+            Field1,
+            List.Select(c => c.ToString()).Aggregate((str1, str2) => str1 + ", " + str2),
+            Child);
       }
    }
 }
